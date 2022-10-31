07cc509a348c73241a2c8bd5a89ffaf286957f51
张传龙 <zclzone@outlook.com>
2022-10-31 11:31:22 +0800

    release v_0.0.2

--- packages/utils/vite.config.ts
+++ packages/utils/vite.config.ts
@@ -1,36 +1,43 @@
-import {defineConfig} from 'vite';
-// import dts from 'vite-plugin-dts'
+import { defineConfig } from 'vite'
+import path from 'path'
 
 export default defineConfig({
-  plugins: [
-    // dts({
-    //   tsConfigFilePath: '../../tsconfig.json',
-    // })
-  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(process.cwd(), 'src'),
+    },
+  },
   build: {
     target: 'modules',
     outDir: 'dist',
     minify: true,
+    sourcemap: false,
     rollupOptions: {
-      input: ['index.ts'],
+      external: ['crypto-js'],
+      input: ['src/index.ts'],
       output: [
         {
           format: 'es',
+          //打包成.js，而不是.es.js
           entryFileNames: '[name].js',
+          //让打包目录和源代码目录对应
           preserveModules: true,
+          //配置打包根目录
           dir: 'dist/es',
         },
         {
           format: 'cjs',
           entryFileNames: '[name].js',
           preserveModules: true,
-          dir: 'dist/lib',
+          dir: 'dist/cjs',
         },
-      ]
+      ],
     },
     lib: {
-      entry: 'index.ts',
-      formats: ['es', 'cjs']
-    }
-  }
+      entry: './src/index.ts',
+      name: '$z',
+      // fileName: '[format]/[name]',
+      // formats: ['es', 'cjs'],
+    },
+  },
 })
