c21bb2645db8a4ef1c60269f843854072039b53d
张传龙 <zclzone@outlook.com>
2023-06-11 15:30:12 +0800

    release

--- packages/utils/src/hooks/useRichText.ts
+++ packages/utils/src/hooks/useRichText.ts
@@ -1,19 +1,29 @@
 import type { App } from 'vue'
 
+function toShadowDom(el: HTMLElement, htmlText: string, styleText = '') {
+  const shadowRoot = el.attachShadow({ mode: 'closed' })
+  shadowRoot.innerHTML = htmlText
+  if (styleText) {
+    // 给富文本添加样式
+    const style = document.createElement('style')
+    style.textContent = styleText
+    shadowRoot.appendChild(style)
+  }
+}
+
 const useRichText = {
   install: (app: App) => {
     app.directive('richText', {
-      mounted(el, binding) {
-        const shadowRoot = el.attachShadow({ mode: 'closed' })
-        const { htmlText, styleText } = binding.value
-        shadowRoot.innerHTML = htmlText
-        if (styleText) {
-          // 给富文本添加样式
-          const style = document.createElement('style')
-          style.textContent = styleText
-          shadowRoot.appendChild(style)
+      updated(el, binding) {
+        let htmlText, styleText
+        if(typeof binding.value === 'object') {
+          htmlText = binding.value.htmlText
+          styleText = binding.value.styleText
+        } else {
+          htmlText = binding.value
         }
-      },
+        toShadowDom(el, htmlText, styleText)
+      }
     })
   },
 }
