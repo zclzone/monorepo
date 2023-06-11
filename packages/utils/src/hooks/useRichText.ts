b98e0a3173b44b8a246292708dd75c0e4ece2b30
张传龙 <zclzone@outlook.com>
2023-06-11 16:23:44 +0800

    release

--- packages/utils/src/hooks/useRichText.ts
+++ packages/utils/src/hooks/useRichText.ts
@@ -1,7 +1,14 @@
 import type { App } from 'vue'
 
+const map = new WeakMap()
 function toShadowDom(el: HTMLElement, htmlText: string, styleText = '') {
-  const shadowRoot = el.attachShadow({ mode: 'closed' })
+  let shadowRoot
+  if (map.get(el)) {
+    shadowRoot = map.get(el)
+  } else {
+    shadowRoot = el.attachShadow({ mode: 'closed' })
+    map.set(el, shadowRoot)
+  }
   shadowRoot.innerHTML = htmlText
   if (styleText) {
     // 给富文本添加样式
@@ -11,19 +18,28 @@ function toShadowDom(el: HTMLElement, htmlText: string, styleText = '') {
   }
 }
 
+function handleBindingVal(binding: any) {
+  let htmlText, styleText
+  if (typeof binding.value === 'object') {
+    htmlText = binding.value.htmlText
+    styleText = binding.value.styleText
+  } else {
+    htmlText = binding.value
+  }
+  return { htmlText, styleText }
+}
+
 const useRichText = {
   install: (app: App) => {
     app.directive('richText', {
-      updated(el, binding) {
-        let htmlText, styleText
-        if(typeof binding.value === 'object') {
-          htmlText = binding.value.htmlText
-          styleText = binding.value.styleText
-        } else {
-          htmlText = binding.value
-        }
+      mounted(el, binding) {
+        const { htmlText, styleText } = handleBindingVal(binding)
         toShadowDom(el, htmlText, styleText)
-      }
+      },
+      updated(el, binding) {
+        const { htmlText, styleText } = handleBindingVal(binding)
+        toShadowDom(el, htmlText, styleText)
+      },
     })
   },
 }
